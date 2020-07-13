79d6e15c8d1723fda9683de3f765365f3b6fe9df
OlgaShrir <OlgaShrir@gmail.com>
2020-07-13 21:42:45 +0300

    Deploy

---
+++ precache-manifest.fad253e8f800f83c4d2d3b0341ee5d67.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da720ffdb86eaccbcfb",
+    "url": "/EvgeniyKoltLaw.github.io/css/app.d02a7e53.css"
+  },
+  {
+    "revision": "c6a125618c0876cf0d0a1604746c47ed",
+    "url": "/EvgeniyKoltLaw.github.io/img/hammer.jpg"
+  },
+  {
+    "revision": "7fb70e647c4e351c39731cfe6eb27e68",
+    "url": "/EvgeniyKoltLaw.github.io/img/logo.png"
+  },
+  {
+    "revision": "af9d694629aeb1f787915e921bdd8e98",
+    "url": "/EvgeniyKoltLaw.github.io/img/portret.af9d6946.jpg"
+  },
+  {
+    "revision": "af9d694629aeb1f787915e921bdd8e98",
+    "url": "/EvgeniyKoltLaw.github.io/img/portret.jpg"
+  },
+  {
+    "revision": "08217e993c9f81eaf406ca96716d4bb8",
+    "url": "/EvgeniyKoltLaw.github.io/index.html"
+  },
+  {
+    "revision": "8da720ffdb86eaccbcfb",
+    "url": "/EvgeniyKoltLaw.github.io/js/app.f36e4894.js"
+  },
+  {
+    "revision": "8ddc208301c87b589b43",
+    "url": "/EvgeniyKoltLaw.github.io/js/chunk-vendors.895e7b2c.js"
+  },
+  {
+    "revision": "a258091b84f7ca3e0cdb0296e2e51c53",
+    "url": "/EvgeniyKoltLaw.github.io/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/EvgeniyKoltLaw.github.io/robots.txt"
+  }
+]);
